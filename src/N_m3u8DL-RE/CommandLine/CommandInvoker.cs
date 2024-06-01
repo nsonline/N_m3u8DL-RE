4fbe557c5e579d8ef61aa299a29b6c4f2537512c
nsx808@hotmail.com <LeoXing>
2024-06-01 22:50:07 +0800

    增加校验选项

--- src/N_m3u8DL-RE/CommandLine/CommandInvoker.cs
+++ src/N_m3u8DL-RE/CommandLine/CommandInvoker.cs
@@ -20,7 +20,7 @@ namespace N_m3u8DL_RE.CommandLine
     {
         public const string VERSION_INFO = "N_m3u8DL-RE (Beta version) 20231113";
 
-        [GeneratedRegex("((best|worst)\\d*|all)")]
+        [GeneratedRegex("((best|worst|optimum)\\d*|all)")]
         private static partial Regex ForStrRegex();
         [GeneratedRegex("(\\d*)-(\\d*)")]
         private static partial Regex RangeRegex();
